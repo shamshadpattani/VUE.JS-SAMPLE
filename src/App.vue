dc312e67dd29dcdf32038d33035d503b3d2bd6a6
shamshadpattani <mail4shamshadpattani@gmail.com>
2021-04-20 18:04:27 +0530

    create a customer page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Customer/> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Customer from './components/Customer'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Customer
   }
 }
 </script>
